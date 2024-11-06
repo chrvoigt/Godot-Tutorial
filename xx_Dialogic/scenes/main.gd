21ea777049264319eaed796adb679256e186539d
Christian Voigt <voigt.cmc@gmail.com>
2024-11-06 16:53:42 +0100

    nope

--- xx_Dialogic/scenes/main.gd
+++ xx_Dialogic/scenes/main.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+@export var path = "res://assets/zapsplat_drumms.mp3"
+@export var music := Dialogic.Audio.current_music_player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic.start('dialog_branch_1') # Replace with function body.
-
+	Dialogic.start('dialog_branch_1') 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	if Dialogic.VAR.response == "x":
+		print (Dialogic.VAR.response)
+		stop_music()
+		
+func stop_music(): 
+	music.stop()
+	
+	 
+	
+	
 	pass
+	
