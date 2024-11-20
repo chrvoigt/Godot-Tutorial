6ea583c8628a414b0386057fc6018c937ad6a386
Christian Voigt <voigt.cmc@gmail.com>
2024-11-20 08:35:03 +0100

    20. Nov

--- xx_Dialogic/scenes/main.gd
+++ xx_Dialogic/scenes/main.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-@export var path = "res://assets/zapsplat_drumms.mp3"
-@export var music := Dialogic.Audio.current_music_player
+@export var path = "res://assets/zapsplat_drumms.mp3" 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,11 +10,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	
 	if Dialogic.VAR.response == "x":
-		print (Dialogic.VAR.response)
+		print (Dialogic.VAR.response) 
 		stop_music()
 		
 func stop_music(): 
-	music.stop()
+	pass
 	
 	 
 	
